79a1326e90adb1d88a69455af5a4b5a61c18d885
satsfy (Renato Britto) <renatobritto@protonmail.com>
2026-05-28 16:40:26 -0300

    openrpc: backport getopenrpcinfo (v23.x)
    Tier-2 with extra adaptations: NONFATAL_UNREACHABLE -> CHECK_NONFATAL(false),
    RPCResult skip_type_check argument removed, SplitString replaced by inline
    pipe split, CRPCCommand registration uses 2-arg + getrpcinfo style.

--- src/rpc/server.h
+++ src/rpc/server.h
@@ -110,6 +110,7 @@ public:
               fn().GetArgNames(),
               intptr_t(fn))
     {
+        this->metadata_fn = fn;
     }
 
     std::string category;
@@ -117,6 +118,7 @@ public:
     Actor actor;
     std::vector<std::string> argNames;
     intptr_t unique_id;
+    RpcMethodFnType metadata_fn{nullptr};
 };
 
 /**
@@ -143,6 +145,8 @@ public:
     * @returns List of registered commands.
     */
     std::vector<std::string> listCommands() const;
+    /** Return a complete OpenRPC 1.3.2 document for all registered commands. */
+    UniValue buildOpenRPCDoc() const;
 
     /**
      * Return all named arguments that need to be converted by the client from string to another JSON type
