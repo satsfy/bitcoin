135543a6b7e10fbf5060ad3a5a9dac8314d967d1
satsfy <renato@local>
2026-07-06 11:37:18 -0300

    openrpc: backport getopenrpcinfo with show_hidden (27.x)

--- src/rpc/server.h
+++ src/rpc/server.h
@@ -13,6 +13,7 @@
 #include <map>
 #include <stdint.h>
 #include <string>
+#include <string_view>
 
 #include <univalue.h>
 
@@ -108,6 +109,7 @@ public:
               fn().GetArgNames(),
               intptr_t(fn))
     {
+        this->metadata_fn = fn;
     }
 
     std::string category;
@@ -124,6 +126,7 @@ public:
     //! appended after other arguments, see transformNamedArguments for details.
     std::vector<std::pair<std::string, bool>> argNames;
     intptr_t unique_id;
+    RpcMethodFnType metadata_fn{nullptr};
 };
 
 /**
@@ -150,6 +153,8 @@ public:
     * @returns List of registered commands.
     */
     std::vector<std::string> listCommands() const;
+    /** Return a complete OpenRPC 1.3.2 document for registered commands. */
+    UniValue buildOpenRPCDoc(bool include_hidden = false) const;
 
     /**
      * Return all named arguments that need to be converted by the client from string to another JSON type
